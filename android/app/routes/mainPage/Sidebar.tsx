14c2ec909227284e0bc5b28d7efcb2901a7cbad2
LazoJankovic <lazar.jankovic@codaxy.com>
2023-07-26 11:58:28 +0200

    add open/close animation to sidebar

--- android/app/routes/mainPage/Sidebar.tsx
+++ android/app/routes/mainPage/Sidebar.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { View, StyleSheet, Animated } from 'react-native';
 
 const Sidebar = ({ sidebarOpened }) => {
-  const sidebarWidth = sidebarOpened ? '65%' : '5%';
+  const sidebarWidth = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(sidebarWidth, {
+      toValue: sidebarOpened ? 65 : 5,
+      duration: 300, 
+      useNativeDriver: false,
+    }).start();
+  }, [sidebarOpened]);
+
+  const interpolatedWidth = sidebarWidth.interpolate({
+    inputRange: [0, 100],
+    outputRange: ['5%', '65%'],
+  });
 
   return (
-    <View style={[styles.container, { width: sidebarWidth }]}>
-      {/* Add your child components here */}
-    </View>
+    <Animated.View style={[styles.container, { width: interpolatedWidth }]}>
+    </Animated.View>
   );
 };
 
